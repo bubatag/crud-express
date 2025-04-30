82db19bbfce38b027c20b909129ddf73c2ebce0e
CristhianHatzman <cristhianhatzman@gmail.com>
2025-04-30 20:39:42 -0300

    feat: Adiciona pagina de coleiras funcional

--- controllers/AnimaisControllers.js
+++ controllers/AnimaisControllers.js
@@ -2,9 +2,10 @@ import express from "express";
 const router = express.Router();
 import Animais from "../models/animais.js";
 
+// SELECT
 router.get("/animais", function (req, res) {
-  // SELECT
-  Animais.findAll().then((animais) => {
+  Animais.findAll()
+    .then((animais) => {
       res.render("animais", {
         animais: animais,
       });
@@ -15,22 +16,24 @@ router.get("/animais", function (req, res) {
 });
 
 // DELETE
-router.get("/animais/delete/:id", (req,res) => {
+router.get("/animais/delete/:id", (req, res) => {
   const id = req.params.id;
   Animais.destroy({
-    where : {
-      id : id,
-    }
-  }).then(() => {
-    console.log("Cliente Excluido com Sucesso!");
-    res.redirect("/animais");
-  }).catch((error) => {
-    console.log(error);
+    where: {
+      id: id,
+    },
   })
-})
+    .then(() => {
+      console.log("Cliente Excluido com Sucesso!");
+      res.redirect("/animais");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 // CREATE
-router.post("/animais/create", (req,res) => {
+router.post("/animais/create", (req, res) => {
   const nome = req.body.nome;
   const raca = req.body.raca;
   const etiqueta = req.body.etiqueta;
@@ -39,17 +42,53 @@ router.post("/animais/create", (req,res) => {
   const coleira = req.body.coleira;
 
   Animais.create({
-    nome : nome,
-    raca : raca,
-    n_etiqueta : etiqueta,
-    idade : idade,
-    sexo : sexo,
-    coleira : coleira,
-  }).then(() => {
-    console.log("Registro efetuado com sucesso!");
-    res.redirect("/animais");
-  }).catch((error) => {
-    console.log(error);
+    nome: nome,
+    raca: raca,
+    n_etiqueta: etiqueta,
+    idade: idade,
+    sexo: sexo,
+    coleira: coleira,
   })
-})
+    .then(() => {
+      console.log("Registro efetuado com sucesso!");
+      res.redirect("/animais");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+
+// PAGE UPDATE
+router.get("/animais/editar/:id", (req,res) => {
+  const id = req.params.id;
+  Animais.findByPk(id).then((animal) => {
+    res.render("animalEditar", {
+      animal : animal,
+    });
+  });
+});
+
+// UPDATE 
+router.post("/animais/update/:id", (req,res) => {
+  const id = req.body.id;
+  const nome = req.body.nome;
+  const raca = req.body.raca;
+  const etiqueta = req.body.etiqueta;
+  const idade = req.body.idade;
+  const sexo = req.body.sexo;
+  const coleira = req.body.coleira;
+  Animais.update({
+    nome: nome,
+    raca: raca,
+    n_etiqueta: etiqueta,
+    idade: idade,
+    sexo: sexo,
+    coleira: coleira,
+  },
+  { where: {id: id}}).then(() => {
+    res.redirect("/animais");
+  });
+});
+
 export default router;
